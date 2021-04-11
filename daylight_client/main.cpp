e87d773c62836a97ec6077d781564513dd4a0f15
jandro1111 <aalatsis@itba.edu.ar>
2021-04-11 10:56:50 -0300

    esto

--- daylight_client/main.cpp
+++ daylight_client/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char* argv[])
 
         tcp::resolver resolver(io_context);
         tcp::resolver::results_type endpoints =
-            resolver.resolve(argv[1], "daytime");
+            resolver.resolve(argv[1], "daytime");//"daytime"
 
         tcp::socket socket(io_context);
         boost::asio::connect(socket, endpoints);
